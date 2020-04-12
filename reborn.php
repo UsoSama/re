2ac73bc01f76d914686816cddbe7a42d30c16654
UsoSama <62829507+UsoSama@users.noreply.github.com>
2020-04-12 10:27:57 +0300

    Add files via upload

---
+++ reborn.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Bakeneko-sama</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/a.css">
+    <link rel="stylesheet" href="css/p.css">
+    <link rel="stylesheet" href="css/circle.css">
+    <link rel="stylesheet" href="css/hr.css">
+    <link rel="stylesheet" href="css/figure.css">
+  </head>
+  <body>
+
+  <? include('additions/header.html'); ?>
+
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-md-12 pt-1 text-md-center">
+        <h2>Re:born</h2> 
+      </div>
+  
+      <div class="col-md-12 mt-3">
+        <h5><p><b>«Re:born»</b> – литературная вселенная, события которой разворачиваются в Великобритании нашего времени.</p>
+        <p>Трагичная история Широ началась в старом доме с зияющей дырой вместо крыши, продолжилась в старшей школе в веселой компании, а закончилась в одиночестве под проливным дождем.</p> 
+        <p><b>«Re:born»</b> можно разделить на три главы: <b>Эфемерная душа, Перерождение и Возвращение к истокам</b></p></h5>
+      </div>
+
+      <div class="col-md-4 mt-5 front">
+        <a href=""><figure><img src="img/1ветка.png" width="100%"></figure></a> 
+      </div>
+  
+      <div class="col-md-4 mt-5 front">
+        <a href=""><figure><img src="img/2ветка.png" width="100%"></figure> </a> 
+      </div>
+  
+      <div class="col-md-4 mt-5 front">
+        <a href=""><figure><img src="img/3ветка.png" width="100%"></figure></a>  
+      </div>
+          
+    </div>
+
+    
+  </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
